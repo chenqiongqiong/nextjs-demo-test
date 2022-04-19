a9f41d2f5578bca28400dd5d1767626a0e554770
陈琼 <619591477@qq.com>
2022-04-19 10:42:50 +0800

    Update getWebpackConfig.js

--- getWebpackConfig.js
+++ getWebpackConfig.js
@@ -8,6 +8,7 @@ process.traceDeprecation = true;
 // Normal requirement
 const path = require('path');
 const webpack = require('webpack');
+
 const WebpackBar = require('webpackbar');
 const webpackMerge = require('webpack-merge');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
